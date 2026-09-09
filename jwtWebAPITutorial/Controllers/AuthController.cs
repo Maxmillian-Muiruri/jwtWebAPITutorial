976d4bab6d82c1cc0f31c41f0c146f3f4f506e4c
Maxmillian-Muiruri <maxmillianmuiruri@gmail.com>
2026-09-09 21:14:13 +0300

    Add project files.

---
+++ jwtWebAPITutorial/Controllers/AuthController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using jwtWebAPITutorial;
+using System.Security.Cryptography;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+
+namespace jwtWebAPITutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        public static User user = new User();
+
+        public IConfiguration Configuration { get; }
+
+        public AuthController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            // Here you would typically handle user registration logic,
+            // such as hashing the password and storing the user in a database.
+            // For demonstration purposes, we'll just return a success message.
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong password.");
+            }
+            // Here you would typically generate a JWT token and return it to the client.
+            // For demonstration purposes, we'll just return a success message.
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };  
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Configuration.GetSection("AppSettings:Secret").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                return computedHash.SequenceEqual(passwordHash);
+
+            }
+        }
+    }
+}
---
+++ jwtWebAPITutorial/User.cs
@@ -0,0 +1,9 @@
+﻿namespace jwtWebAPITutorial
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash  { get; set; } 
+        public byte[] PasswordSalt { get; set; }
+    }
+}
